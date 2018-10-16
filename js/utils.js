146c0d2431fe6033bc97e0d4fca8e067838b04e2
Charley Ho <potatoarecool@gmail.com>
2018-10-16 14:00:50 -0500

    Updated with more bugfixes & minor content tweaks 1. no longer have lines going to -0.5 2. added tests for heuristic verification 3. hide cards when there aren't enough spaces 4. changed content a bit

--- js/utils.js
+++ js/utils.js
@@ -1,82 +1,69 @@
+/**
+ * Util Functions for Mondrian Generation
+ * inc. class def for Mondrian, Square, Line, Point
+ */
 
-function getRand(startRange,endRange) {
+
+function getRand(startRange, endRange) {
     if (endRange < startRange) {
         throw new Error('can not get rand of negative range');
     }
-   return Math.floor(Math.random()*(endRange-startRange+1))+startRange;
+    return Math.floor(Math.random() * (endRange - startRange + 1)) + startRange;
 }
 
 function Mondrian(width, height) {
-   if (!width || !height) {
-      throw new Error("Mondrian must have width & height", width, height);
-   }
-   this.width = width;
-   this.height = height;
+    if (!width || !height) {
+        throw new Error("Mondrian must have width & height", width, height);
+    }
+    this.width = width;
+    this.height = height;
 
-   this.lines = [];
+    this.lines = [];
 
-   this.addLine = function(line) {
-      this.lines.push(line);
-   };
+    this.addLine = function (line) {
+        this.lines.push(line);
+    };
 
-   this.squares = [];
+    this.squares = [];
 
-   this.addSquare = function(square) {
-      this.squares.push(square);
-   }
+    this.addSquare = function (square) {
+        this.squares.push(square);
+    }
 }
 function Square(startPoint, endPoint) {
-   if (!startPoint || !endPoint) {
-      throw new Error("square must have two points", startPoint, endPoint);
-   }
-   this.start = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? startPoint : endPoint;
-   this.end = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? endPoint : startPoint;
+    if (!startPoint || !endPoint) {
+        throw new Error("square must have two points", startPoint, endPoint);
+    }
+    this.start = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? startPoint : endPoint;
+    this.end = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? endPoint : startPoint;
 
-   this.width = endPoint.x - startPoint.x;
-   this.height = endPoint.y - startPoint.y;
-   this.area = this.width * this.height;
-   this.filled = false;
-   this.color = undefined;
+    this.width = endPoint.x - startPoint.x;
+    this.height = endPoint.y - startPoint.y;
+    this.area = this.width * this.height;
+    this.filled = false;
+    this.color = undefined;
 }
 
 function Line(startPoint, endPoint) {
-   if (!startPoint || !endPoint) {
-      throw new Error("line must have two points", startPoint, endPoint);
-   }
-   this.start = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? startPoint : endPoint;
-   this.end = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? endPoint : startPoint;
+    if (!startPoint || !endPoint) {
+        throw new Error("line must have two points", startPoint, endPoint);
+    }
+    this.start = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? startPoint : endPoint;
+    this.end = (startPoint.x <= endPoint.x && startPoint.y <= endPoint.y) ? endPoint : startPoint;
 
-   this.length = Math.sqrt(
-      Math.pow(this.end.y-this.start.y,2) +
-      Math.pow(this.end.x-this.start.x,2));
+    this.length = function () {
+        return Math.sqrt(
+            Math.pow(this.end.y - this.start.y, 2) +
+            Math.pow(this.end.x - this.start.x, 2));
+    };
 
-   this.isHorizontal = this.end.y === this.start.y;
+    this.isHorizontal = this.end.y === this.start.y;
 }
 
-function Point(x,y) {
-   if (!_.isNumber(x) || !_.isNumber(y)) {
-      throw new Error("point must have x and y coords", x, y);
-   }
-   this.x = x;
-   this.y = y;
-}
-
-var defaultOpts = {
-   minHLines: 2,
-   maxHLines: 5,
-   minVLines: 1,
-   maxVLines: 3,
-   minShortLines: 3,
-   maxShortLines: 7,
-   minSquares: 4,
-   maxSquares: 6,
-   numUsableSquares: 6,
-};
-
-var narrowOpts = {
-   minHLines: 7,
-   maxHLines: 9,
-   minVLines: 5,
-   maxVLines: 7,
-   minShortLines: 2,
+function Point(x, y) {
+    if (!_.isNumber(x) || !_.isNumber(y)) {
+        throw new Error("point must have x and y coords", x, y);
+    }
+    this.x = x;
+    this.y = y;
 }
